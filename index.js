e91b58a5d38d87bfbd38b4632d68b282977669fd
Michael Bonner <michaelbonner413@gmail.com>
2024-10-22 00:29:42 -0400

    Lloyd gif

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 
 
-const getData = () => {
-    fetch (`https://dictionaryapi.dev/=${inputValue}`)
+const getData = (inputValue) => {
+    // let inputValue = searchData();
+    fetch (`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
     .then(response => response.json())
-    .then(jsonData => console.log(jsonData))
-    return jsonData
+    .then(jsonResponse => {
+        document.querySelector("body").innerHTML +=
+        // `<ul>
+        //     <li>Word: $(jsonResponse.word)</li>
+        //     <li>Part Of Speech: $(jsonResponse.meanings.partOfSpeech)</li>
+        //     <li>Definition: $(jsonResponse.meanings.definition)</li>
+        //     <li>Synonyms: $(jsonResponse.meanings.synonyms)</li>
+        // </ul>`
+        jsonResponse[0].word
+        console.log(jsonResponse[0].word);
+})
+    // .catch(error => console.error('Error fetching data:', error));
+}
+const searchData = () => {
+    let inputValue = document.querySelector("#searchField").value;
+    return inputValue;
 }
 
-const searchData = () => {
-    let inputValue = document.querySelector("#searchField").v
-    return inputValue
-}
-console.log(jsonData)
+
