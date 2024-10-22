6ec71cc341cf3803c81d64e664274c3ab18016fc
Michael Bonner <michaelbonner413@gmail.com>
2024-10-22 11:41:15 -0400

    MVP

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const getData = (inputValue) => {
     fetch (`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
     .then(response => response.json())
     .then(jsonResponse => {
-        document.querySelector("body").innerHTML +=
-        // `<ul>
-        //     <li>Word: $(jsonResponse.word)</li>
-        //     <li>Part Of Speech: $(jsonResponse.meanings.partOfSpeech)</li>
-        //     <li>Definition: $(jsonResponse.meanings.definition)</li>
-        //     <li>Synonyms: $(jsonResponse.meanings.synonyms)</li>
-        // </ul>`
-        jsonResponse[0].word
+        document.querySelector("#newList").innerHTML =
+        `<ul>
+            <li>Word: ${jsonResponse[0].word}</li>
+            <li>Part Of Speech: ${jsonResponse[0].meanings[0].partOfSpeech}</li>
+            <li>Definition: ${jsonResponse[0].meanings[0].definitions[0].definition}</li>
+            <li>Synonyms: ${jsonResponse[0].meanings[0].synonyms}</li>
+        </ul>`
+        
         console.log(jsonResponse[0].word);
 })
     // .catch(error => console.error('Error fetching data:', error));
